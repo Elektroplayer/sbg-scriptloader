3c24311c4230956a9a3be6f371b5046a99239965
Electroplayer <vitaly.hawanski@yandex.ru>
2023-05-06 17:47:05 +0300

    Commit

--- index.js
+++ index.js
@@ -8,6 +8,6 @@
     */
     // let scripsFromRepo;
     let data = await $.get('https://raw.githubusercontent.com/Elektroplayer/sbg-scriptloader/main/repo.txt');
-    let scripsFromRepo = data.split("\n").map(elm => {return {name: elm[0], author: elm[1], url: elm[2]}})
+    let scripsFromRepo = data.split("\n").map(elm => {return {name: elm.split(":")[0], author: elm.split(":")[1], url: elm.split(":")[2]}})
     console.log(scripsFromRepo);
 })()
