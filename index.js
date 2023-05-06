848261ccf1ce71d3c5bc2d0f6e680da772cb58f6
Electroplayer <vitaly.hawanski@yandex.ru>
2023-05-06 18:10:28 +0300

    Commit

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 (async function() {
-    /*
-        {
-            name: string,
-            author: string
-            url: string
+    function collectionContains(collection, url) {
+        for (var i = 0; i < collection.length; i++) {
+            if( collection[i].src == url ) return true;
         }
-    */
-    // let scripsFromRepo;
+        return false;
+    }
+
     let data = await $.get('https://raw.githubusercontent.com/Elektroplayer/sbg-scriptloader/main/repo.txt');
-    let scripsFromRepo = data.split("\n").map(elm => {return {name: elm.split(":")[0], author: elm.split(":")[1], url: elm.split(":")[2]}})
-    console.log(scripsFromRepo);
+    let repo = data.split("\n").map(elm => {
+        let nelm = elm.split(":");
+        return {
+            name: nelm[0], 
+            author: nelm[1], 
+            url: `https://${nelm[1]}.github.io/${nelm[2]}`,
+            active: !!localStorage.installedScripts?.find(elm => elm == `https://${nelm[1]}.github.io/${nelm[2]}`)
+        };
+    });
+
+    if(data.endsWith("\n")) repo.pop();
+
+    if(!localStorage.c) localStorage.installedScripts = [];
+
+    var parent = document.getElementsByTagName('head').item(0);
+    let scriptElements = parent.getElementsByTagName('script');
+    let script;
+
+    for(let i = 0;i<localStorage.installedScripts.length;i++) {
+        if(collectionContains(scriptElements, localStorage.installedScripts[i])) return;
+
+        script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.setAttribute('src', localStorage.installedScripts[i]);
+        script.setAttribute('async', '');
+        parent.appendChild(script);
+    };
 })()
