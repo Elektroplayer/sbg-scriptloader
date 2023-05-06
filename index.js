9ca75a9b15f4d9a7a00b1372bebad47d8d395f67
Electroplayer <vitaly.hawanski@yandex.ru>
2023-05-06 17:41:03 +0300

    commit

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-(function() {
-    $.get('https://raw.githubusercontent.com/Elektroplayer/sbg-scriptloader/main/repo.txt', function(data) {
-        alert(data);
-    });
+(async function() {
+    /*
+        {
+            name: string,
+            author: string
+            url: string
+        }
+    */
+    // let scripsFromRepo;
+    let data = await $.get('https://raw.githubusercontent.com/Elektroplayer/sbg-scriptloader/main/repo.txt');
+    let scripsFromRepo = data.split("\n").map(elm => {return {name: elm[0], author: elm[1], url: elm[2]}})
+    console.log(scripsFromRepo);
 })()
